9dbafdd8f8e8f1a21b45dbf084369b488d8a92d7
Sachin Devdhar <sachdevdhar@gmail.com>
2022-01-16 22:08:46 -0500

    complete functionlaity of class based components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import News from "./components/News";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import LoadingBar from "react-top-loading-bar";
+export default class App extends Component {
+  pageSize = 5;
+  apiKey = process.env.REACT_APP_APIKEY;
+
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (prog) => {
+    this.setState({ progress: prog });
+  };
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <LoadingBar
+          color="#f11946"
+          height={3}
+          progress={this.state.progress}
+          onLoaderFinished={() => this.setProgress(0)}
+        />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="general"
+                country="in"
+                category="general"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/general"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="general"
+                country="in"
+                category="general"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="technology"
+                country="in"
+                category="technology"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="business"
+                country="in"
+                category="business"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="health"
+                country="in"
+                category="health"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="science"
+                country="in"
+                category="science"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="sports"
+                country="in"
+                category="sports"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={this.setProgress}
+                apiKey={this.apiKey}
+                key="entertainment"
+                country="in"
+                category="entertainment"
+                pageSize={this.pageSize}
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageURL, newsURL, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              !imageURL
+                ? "https://bitsofco.de/content/images/2018/12/broken-1.png"
+                : imageURL
+            }
+            className="card-img-top"
+            alt="Not available"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author} on {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsURL}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
